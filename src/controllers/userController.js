2fc0b869091a05699caa3796c0c81a5444cfe02b
jatinkumar2062000 <jatinkumar2062000@gmail.com>
2022-10-11 11:27:56 +0530

    login and user and update done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -116,6 +116,32 @@ const createUser = async function (req, res) {
     }
 }
 
+const userLogin = async (req, res)=>{
+    try{
+        const data =  req.body;
+   const  {email, password} = data
+   if(Object.keys(data).length == 0) return res.status(400).send({status:false, message:"please enter data in reqbody"})
+   
+   if(!email) return res.status(400).send({status:false, message:"email not present"})
+   if(!password) return res.status(400).send({status:false, message:"password not present"})
+
+ if(!isValidEmail(email))return res.status(400).send({status:false, message:"email is not correct formate"})
+ if(!isValidPassword(password))return res.status(400).send({status:false, message:"password is not correct formate"})
+
+ const findUser = await userModel.findOne({email:email, password:password})
+ if(!findUser) return res.send(404).send({status:false, message: "email or password is incorrect"})
+
+ const payload = { userId: user._id, iat: Math.floor(Date.now() / 1000) };
+
+   const token = jwt.sign(payload, "group45")
+   return res.status(200).send({ status: true, message: "User login successfully", token: token, exp: payload.exp, });
+} catch (err) {
+  res.status(500).send({ status: false, message: err.message });
+}};
+
+ 
+
+
 const updateProfile = async function (req, res) {
     try {
         const body = req.body;
@@ -197,4 +223,4 @@ const updateProfile = async function (req, res) {
     }
 }
 
-module.exports = { createUser, updateProfile }
+module.exports = { createUser, updateProfile, userLogin }

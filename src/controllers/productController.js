02ab7076c00e775b2455f6b08716b7f752c18f85
jatinkumar2062000 <jatinkumar2062000@gmail.com>
2022-10-13 11:17:50 +0530

    delete

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,43 +3,559 @@ const uploadFile = require("../aws/aws")
 
 const {isValid, isValidRequestBody, isValidEnum } = require('../validator/validator')
 
-const createProduct = async (req, res)=>{
+const createProduct = async (req, res)=>
+{
     try{
         const files = req.files
+
         const data = req.body
-        const {title, description, price, isFreeShipping, productImage, style,
-             availableSizes, installments} = data
+
+        const {title, description, price, isFreeShipping, productImage, style,availableSizes, installments} = data
 
     if(!isValid(title)) {return res.status(400).send({status:false, message: "title required"})}
+
     if(!isValid(description)) {return res.status(400).send({status:false, message: "description required"})}
+
     if(!isValid(price)) {return res.status(400).send({status:false, message: "price required"})}
+
     //  if(!isValid(currencyId)) {return res.status(400).send({status:false, message: "currencyId required"})}
+
     //    if(!isValid(currencyFormat)) {return res.status(400).send({status:false, message: "currencyFormat required"})}
+
     if(!isValid(isFreeShipping)) {return res.status(400).send({status:false, message: "isFreeShipping required"})}
+
   //  if(!isValid(productImage)) {return res.status(400).send({status:false, message: "productImage required"})}
+
     if(!isValid(style)) {return res.status(400).send({status:false, message: "style required"})}
+
     if(!isValid(availableSizes)) {return res.status(400).send({status:false, message: "availableSizes required"})}
+
     if(!isValid(installments)) {return res.status(400).send({status:false, message: "installments required"})}
+
+    // if(!productImage) {return res.status(400).send({status:false, message: "productImage is required"})}
 
     data.currencyFormat = "₹"
     data.currencyId = "INR"
 
-    if(!isValidRequestBody(data)) {return res.status(400).send({ status: false, Message: 
-        "Invalid request parameters, Please provide product details" })}
+    if(!isValidRequestBody(data)) {return res.status(400).send({ status: false, Message: "Invalid request parameters, Please provide product details" })}
 
     if(files.length > 0) {data.productImage = await uploadFile(files[0]);
-        } else {return res.status(400).send({ status: false, message: "ProductImage  is required" });}
+    } 
+        else 
+    {
+        return res.status(400).send({ status: false, message: "ProductImage  is required" });
+    }
 
-   const uniqueTitle = await productModel.findOne({title:title})
+    const uniqueTitle = await productModel.findOne({title:title})
+    
     if(uniqueTitle){return res.status(400).send({status:false, message: "title must be unique"})}
 
     if (isValidEnum(availableSizes))
       return res.status(400).send({ status: false, msg: "availableSizes should be of (S,XS,M,X,L,XXL,XL)" });
 
-      const saveData = await productModel.create(data)
-      res.status(201).send({ status: true, message: 'product created successfully', data: saveData })
-    } catch (err) {
+    const saveData = await productModel.create(data)
+
+    res.status(201).send({ status: true, message: 'product created successfully', data: saveData })
+
+    } 
+
+    catch (err) 
+    {
         return res.status(500).send({ status: false, message: err.message });
     }
 }
  module.exports = {createProduct}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ let deleteByIDProduct = async (req, res) => 
+ {
+    try {
+        const productId = req.params.productId;
+
+        if (!mongoose.Types.ObjectId.isValid(productId)) 
+        
+        {
+            return res.status(400).send({ status: false, message: "NOT A VALID ID" })
+        }
+
+
+        const product = await productModel.findOne({ _id: productId, isDeleted: false, deletedAt: null });
+
+        if (!product)
+        
+        {
+            return res.status(404).send({ status: false, message: "NO PRODUCT FOUND" });
+        }
+
+        const DELETE = await productModel.findByIdAndUpdate(productId, { $set: { isDeleted: true, deletedAt: Date.now() } });
+
+        res.status(200).send({ status: true, message: `PRODUCT WITH ID ${productId} DELETED` });
+
+        }
+        catch (error) 
+        {
+
+        res.status(500).send({ status: false, message: error.message });
+    }
+}
+
+
+module.exports={deleteByIDProduct}

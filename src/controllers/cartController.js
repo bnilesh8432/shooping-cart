1d196b63b54cd26e4f93ec06950c5b201aed9887
ziyaurrahman01 <ziyaurrahmankhan01@gmail.com>
2022-10-18 01:34:34 +0530

    rahman's code

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -17,4 +17,49 @@ return res.status(204).send({status: false, message: "deleted successfully"})
     }
 }
 
-module.export = {deleteByUserId}
+//====================put cart-api==================================
+
+const updateCart = async (req, res) => {
+    try {
+        const data = req.body;
+        const userId = req.params.userId;
+        const { cartId, productId} = data;
+
+        if (!isValidRequestBody(data)) {
+            return res.status(400).send({ status: false, message: "Please provide some data to update" })}
+
+        if (!isValidObjectId(userId)) {
+            return res.status(400).send({ status: false, message: "Invalid UserId" })}
+
+        if (userId != req.userId) {
+            return res.status(403).send({ status: false, message: "Unauthorized Access" });
+        }
+
+        let userExist = await userModel.findById(userId);
+        if (!userExist) {
+            return res.status(404).send({ status: false, message: "No User Found With this Id" })}
+
+        if (!productId) {return res.status(400).send({ status: false, message: "Provide the ProductId" })}
+
+        if (!isValidObjectId(productId)) {return res.status(400).send({ status: false, message: "Invalid ProductId"})}
+
+        let product = await productModel.findById(productId);
+        if (!product) {
+            return res.status(404).send({ status: false, message: `No Product Found With this ${productId}` });
+        }
+        if (!cartId) {
+            return res.status(400).send({ status: false, message: "Provide the carrId" });
+        }
+        if (!isValidObjectId(cartId)) {
+            return res.status(400).send({ status: false, message: "Invalid cartId" });
+        }
+
+        let cartExist = await cartModel.findById(cartId);
+
+        if (cartExist) {
+        if(cartExist.items.length == 0) {return res.status(400).send({ status: false, message: "your cart is Empty"})}
+          }
+        } catch (err) {
+            return res.status(500).send({ status: false, message: err.message });
+        }}
+module.export = {updateCart, deleteByUserId}

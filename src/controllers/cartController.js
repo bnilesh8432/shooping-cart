de3d7e830e21bee216077cd0dd93b4595e82e30a
s123tuti <stutisharmaofficial@gmail.com>
2022-10-18 22:47:58 +0530

    deletebyuserid

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -182,21 +182,25 @@ const updateCart = async (req, res) => {
 
 //================================================================================================================================================
 
-        const deleteByUserId = async (req, res) => {
-            try {
-        let userId = req.params.userId
-        let findCart = await cartModel.findOne({userId: userId})
-        if(findCart.items.length == 0){
-        return res.status(400).send({status: false, message: "cart is already empty"})
-        }
-        await cartModel.updateOne({_id: findCart._id}),
-        {items: [], totalPrice: 0, totalitem: 0}
-        return res.status(204).send({status: false, message: "deleted successfully"})
-        }      
-            catch (err){
-                return res.status(500).send({status: false, message: err.message})
-            }
-        }
+const deleteByUserId = async (req, res) => {
+  try {
+let userId = req.params.userId
+if(!mongoose.isValidObjectId(userId)){
+return res.status(400).send({status: false, message: "provide valid userId"})
+}
+let findCart = await cartModel.findOne({userId: userId})
+if(findCart.items.length == 0){
+return res.status(400).send({status: false, message: "cart is already empty"})
+}
+await cartModel.updateOne({_id: findCart._id},
+{items: [], totalPrice: 0, totalItems: 0}
+)
+return res.status(204).send({status: false, message: "deleted successfully"})
+}      
+  catch (err){
+      return res.status(500).send({status: false, message: err.message})
+  }
+}
 
 
 module.exports = {updateCart, deleteByUserId,createCart,getCart}

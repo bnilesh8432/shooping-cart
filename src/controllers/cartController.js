935d9621b01897e85a8aa13174d1d7c086bffd4d
jatinkumar2062000 <jatinkumar2062000@gmail.com>
2022-10-18 11:23:27 +0530

    createcart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,6 +1,75 @@
 const cartModel = require('../models/cartModel')
 const mongoose = require('mongoose')
 
+
+
+const createCart = async function (req, res) {
+    try {
+        let userId = req.params.userId
+        let { productId } = req.body
+        if (!productId)
+            return res.status(400).send({ status: false, message: "Enter productId for the product to be added to cart." })
+  
+      //   if (!validator.isValidObjectId(productId))
+      //       return res.status(400).send({ status: false, message: "productId is invalid" })
+  
+        if (!mongoose.Types.ObjectId.isValid(productId)) {
+          return res.status(400).send({
+              status: false,
+              message: "NOT A VALID ID"
+          })
+      }
+      let 
+        const product = await productModel.findOne({ _id: productId, isDeleted: false })
+        if (!product)
+            return res.status(404).send({ status: false, message: "Product not found" })
+  
+        let cart = await cartModel.findOne({ userId })
+        if (!cart) {
+            let newCart = await cartModel.create(
+                {
+                    userId: userId,
+                    items: [{
+                        productId: productId,
+                        quantity: 1
+                    }],
+                    totalPrice: product.price,
+                    totalItems: 1
+                }
+            )
+            return res.status(201).send({ status: true, message: "Success", data: newCart })
+        }
+        let indexOfProduct = -1
+        for (let i in cart.items) {
+            if (cart.items[i].productId == productId) {
+                indexOfProduct = i
+                break
+            }
+        }
+        if (indexOfProduct == -1) {
+            cart = await cartModel.findOneAndUpdate(
+                { userId },
+                {
+                    $addToSet: { items: { productId: productId, quantity: 1 } },
+                    $inc: { totalPrice: product.price, totalItems: 1 }
+                },
+                { new: true }
+            )
+        }
+        else {
+            ++cart.items[indexOfProduct].quantity
+            cart.totalPrice += product.price
+            await cart.save()
+        }
+        return res.status(201).send({ status: true, message: "Success", data: cart })
+  
+    } 
+    catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+    }
+  }
+
+
 const deleteByUserId = async (req, res) => {
     try {
 let userId = req.params.userId
@@ -62,4 +131,6 @@ const updateCart = async (req, res) => {
         } catch (err) {
             return res.status(500).send({ status: false, message: err.message });
         }}
-module.export = {updateCart, deleteByUserId}
+
+        
+module.export = {updateCart, deleteByUserId,createCart}
